bc6e9b8378627b97453db5d5b0c34f12d6db19cb
Anatoliy Varanitsa <prizrack13@gmail.com>
2026-04-24 21:27:42 +0200

    Init

---
+++ spec/interactor/extended_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Interactor::Extended do
+  it "has a version number" do
+    expect(Interactor::Extended::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
